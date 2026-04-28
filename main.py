d2ea07f06c7075e5100ae49ed844a859b531de72
danilHarr <danil_gusev2004@bk.ru>
2026-04-29 00:49:06 +0300

    Add some function

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI
-from fastapi.responses import Response # Формирует ответ
+from fastapi import FastAPI, HTTPException
 
 # Инициализация FastAPI приложения
 app = FastAPI()
 
 
-@app.get("/health")
-def health_check():
-    return Response(status_code=200)
+BALANCE = {}
+
+# Query parametr
+@app.get("/balance")
+def get_balance(wallet_name: str | None = None):
+    if wallet_name is None:
+        return {"total_balance": sum(BALANCE.values())}
+    if wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{wallet_name} not found'"
+        )
+    return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
+
+
+# Path parametr
+@app.post("wallets/{name}")
+def receive_money(name: str, amount: int):
+    if name not in BALANCE:
+        BALANCE[name] = 0
+    BALANCE[name] += amount
+
+    return {
+        "message": f"Added {amount} to {name}",
+        "wallet": name,
+        "new_balance": BALANCE[name]
+    }
