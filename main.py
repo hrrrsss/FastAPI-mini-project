3fdba58ea049e6fbf616c727c8436388602403e4
danilHarr <danil_gusev2004@bk.ru>
2026-04-30 00:35:36 +0300

    Created project structure

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+
+from app.api.v1.wallets import router as wallet_router
+from app.api.v1.operations import router as operations_router
 
 # Инициализация FastAPI приложения
 app = FastAPI()
 
-
-BALANCE = {}
-
-# Query parametr
-@app.get("/balance")
-def get_balance(wallet_name: str | None = None):
-    if wallet_name is None:
-        return {"total_balance": sum(BALANCE.values())}
-    if wallet_name not in BALANCE:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Wallet '{wallet_name}' not found"
-        )
-    return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
-
-
-# Path parametr
-@app.post("/wallets/{name}")
-def create_wallet(name: str, initial_balance: float = 0):
-    if name in BALANCE:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Wallet '{name}' already exists"
-        )
-
-    BALANCE[name] = initial_balance
-    return {
-        "message": f"Wallet '{name}' created",
-        "wallet": name,
-        "balance": BALANCE[name]
-    }
+app.include_router(wallet_router, prefix="/api/v1", tags=["wallet"])
+app.include_router(operations_router, prefix="/api/v1", tags=["operations"])
